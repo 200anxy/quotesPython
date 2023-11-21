4ac16a6c0d9046ee64ed90d084d7204eebd34466
Aadarsh <126645650+200xy@users.noreply.github.com>
2023-11-21 17:22:40 +1100

    Add files via upload
    Signed-off-by: Aadarsh <126645650+200xy@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+file_paths = r"C:\Users\Aadarsh Nair\Desktop\Aadarsh_Code\Quotes\Quotes.txt"
+
+# Usage
+
+x = open(file_paths, 'r', encoding='utf-8')
+content = x.read()
+quotes = content.split(';')  # Split the content by ';'
+random_quote = random.choice(quotes)
+
+print(random_quote)
+x.close()
+    
+
+
